99bb4d77cde65cc4784b3f6b46d779ac37595a83
Jonathan Oddy <jonathan@woaf.net>
2014-11-15 23:46:39 +0000

    Fix warnings about default timezone in example.

--- examples/packManipulator.php
+++ examples/packManipulator.php
@@ -11,6 +11,9 @@ use \MooPhp\MooInterface\Data as Data;
  * @copyright Copyright (c) 2012, Moo Print Ltd.
  */
 
+if (!ini_get("date.timezone")) {
+    date_default_timezone_set("UTC");
+}
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
